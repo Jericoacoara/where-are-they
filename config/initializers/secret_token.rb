479ceac81a570d1be40f780050500aab32be16db
Cihan Toraman <cihantoraman14@gmail.com>
2011-03-17 22:57:40 +0200

    where are they test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wherearethey::Application.config.secret_token = 'b37a44f323b5a78ed33d0dffba81fbc9e1652c49c27692c16b5d53d06e69104fb70ba0dcfa051d5257b5802f58e2399351ff5e29608398c1181e9ddddb5deec3'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Wherearethey::Application.routes.draw do
+
+ resources :users
+
+  root :to => "users#index"
+
+end
